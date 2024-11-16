4e4e5ecdbf52a88088018fe436d27d6f54395ada
Rifat <hrifat450@gmail.com>
2024-11-16 21:37:38 +0600

    connection pooling added in db

--- util/db.go
+++ util/db.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"log"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,7 +10,7 @@ import (
 
 var DB *gorm.DB // Global variable for the DB connection
 
-// ConnectDB initializes the global DB connection
+// ConnectDB initializes the global DB connection with connection pooling
 func ConnectDB(dsn string) error {
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -17,5 +18,18 @@ func ConnectDB(dsn string) error {
 		log.Fatalf("Could not connect to the database: %v", err)
 		return err
 	}
+
+	// Get the underlying sql.DB instance to configure connection pooling
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("Could not get sql.DB from Gorm DB: %v", err)
+		return err
+	}
+
+	// Configure the connection pool
+	sqlDB.SetMaxOpenConns(50)                 // Set the maximum number of open connections
+	sqlDB.SetMaxIdleConns(25)                 // Set the maximum number of idle connections
+	sqlDB.SetConnMaxLifetime(5 * time.Minute) // Set the maximum connection lifetime
+
 	return nil
 }
